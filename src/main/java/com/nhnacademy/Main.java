e61a11eb9da528dbe971cdba9f9f7ad7c92cfdcd
ysh011130 <ysh011130@naver.com>
2025-12-26 17:47:36 +0900

    step03 다른버전

--- src/main/java/com/nhnacademy/Main.java
+++ src/main/java/com/nhnacademy/Main.java
@@ -5,35 +5,48 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         
-        String sel = "";
-        while (true) {
+        boolean quit = false;
+        String input = "";
+
+        while (!quit) {
             System.out.println("=== TODO 앱===");
             System.out.println("1. 등록");
             System.out.println("2. 조회");
             System.out.println("0. 종료");
             System.out.print("선택 > ");
-            sel = reader.readLine();
 
-            switch (sel) {
-                case "1":
-                    System.out.println("[등록] 메뉴 선택됨");
-                    break;
-                case "2":
-                    System.out.println("[조회] 메뉴 선택됨");
-                    break;
-                case "0":
-                    System.out.println("프로그램을 종료합니다.");
-                    reader.close();
-                    return;
-                default:
-                    System.out.println("잘못된 입력입니다.");
-                    break;
+            try {
+                input = reader.readLine();
+
+                switch (input) {
+                    case "1":
+                        System.out.println("[등록] 메뉴 선택됨");
+                        break;
+                    case "2":
+                        System.out.println("[조회] 메뉴 선택됨");
+                        break;
+                    case "0":
+                        System.out.println("프로그램을 종료합니다.");
+                        quit = true;
+                        break;
+                    default:
+                        System.out.println("잘못된 입력입니다.");
+                        break;
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
             }
 
             System.out.println();
+        }
+        
+        try {
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
