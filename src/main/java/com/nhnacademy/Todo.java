d9b4358ce6290b995d4a85b46f4fca0d4f5e65bb
ysh011130 <ysh011130@naver.com>
2025-12-29 16:58:43 +0900

    step04 : main() fixed

--- src/main/java/com/nhnacademy/Todo.java
+++ src/main/java/com/nhnacademy/Todo.java
@@ -1,5 +1,9 @@
 package com.nhnacademy;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Todo {
     private String title;
     private int hours;
@@ -49,8 +53,23 @@ public class Todo {
         return status + " " + this.title + " (" + this.hours + "시간)";
     }
 
-    public static void main(String[] args) {
-        Todo todo = new Todo("Java Study", 3, false);
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("Todo 작성");
+        System.out.println("입력 예시 : Java,3,false");
+        System.out.print("> ");
+        String input = reader.readLine();
+        input = input.replaceAll("\\s+", "");
+        String[] tmp = input.split(",");
+
+        Todo todo = new Todo(
+            tmp[0],
+            Integer.parseInt(tmp[1]),
+            Boolean.parseBoolean(tmp[2])
+        );
         System.out.println(todo);
+
+        reader.close();
     }
 }
