303dad19b2a9c1c9d712e7c3bf80ab3fb0c64cb8
ysh011130 <ysh011130@naver.com>
2025-12-31 10:33:53 +0900

    step06

--- src/main/java/com/nhnacademy/todo/Main.java
+++ src/main/java/com/nhnacademy/todo/Main.java
@@ -3,58 +3,40 @@ package com.nhnacademy.todo;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 import com.nhnacademy.todo.model.Todo;
+import com.nhnacademy.todo.service.TodoService;
 
 public class Main {
+    public static void printMenu() {
+        System.out.println("=== TODO 앱===");
+        System.out.println("1. 등록  2. 조회  0. 종료");
+        System.out.print("선택 > ");
+    }
+
     public static void main(String[] args) throws IOException{
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        ArrayList<Todo> todoList = new ArrayList<>();
+        TodoService service = new TodoService();
 
-        String sel = "";
         while (true) {
-            System.out.println("=== TODO 앱===");
-            System.out.println("1. 등록");
-            System.out.println("2. 조회");
-            System.out.println("0. 종료");
-            System.out.print("선택 > ");
-            sel = reader.readLine();
+            printMenu();
+            String choice = reader.readLine();
 
-            switch (sel) {
+            switch (choice) {
                 case "1":
                     // "[등록] 메뉴 선택됨"
-                    System.out.println();
+                    System.out.print("할 일: ");
+                    String title = reader.readLine();
+                    System.out.print("시간: ");
+                    int hours = Integer.parseInt(reader.readLine());
 
-                    System.out.println("Todo 작성");
-                    System.out.println("입력 예시 : Java,3,false");
-                    System.out.print("> ");
-                    String input = reader.readLine();
-                    input = input.replaceAll("\\s+", "");
-                    String[] tmp = input.split(",");
-
-                    todoList.add(
-                        new Todo(
-                            tmp[0],
-                            Integer.parseInt(tmp[1]),
-                            Boolean.parseBoolean(tmp[2])
-                        )
-                    );
+                    Todo todo = new Todo(title, hours, false);
+                    service.add(todo);
 
                     break;
                 case "2":
                     // "[조회] 메뉴 선택됨"
-                    System.out.println();
-                    
-                    if (todoList.size() == 0) {
-                        System.out.println("목록이 비어있습니다.");
-                        break;
-                    }
-
-                    System.out.println("=== TODO 목록 ===");
-                    for (int i = 0; i < todoList.size(); i++) {
-                        System.out.println((i+1) + ". " + todoList.get(i));
-                    }
+                    service.printAll();
                     break;
                 case "0":
                     System.out.println("프로그램을 종료합니다.");
--- src/main/java/com/nhnacademy/todo/service/TodoService.java
+++ src/main/java/com/nhnacademy/todo/service/TodoService.java
@@ -1,5 +1,37 @@
 package com.nhnacademy.todo.service;
 
+import com.nhnacademy.todo.model.Todo;
+import java.util.ArrayList;
+
 public class TodoService {
-    
+    private ArrayList<Todo> todoList = new ArrayList<>();
+
+    public void add(Todo todo) {    // 등록
+        todoList.add(todo);
+        System.out.println("등록 완료: " + todo.getTitle());
+    }
+
+    public void delete(int index) { // 삭제
+        todoList.remove(index);
+    }
+
+    public Todo get(int index) {    // 조회
+        return todoList.get(index);
+    }
+
+    public ArrayList<Todo> getAll() {   // 전체 조회
+        return todoList;
+    }
+
+    public void printAll() {
+        if (todoList.isEmpty()) {
+            System.out.println("등록된 TODO가 없습니다.");
+            return;
+        }
+
+        System.out.println("=== TODO 목록 ===");
+        for (int i = 0; i < todoList.size(); i++) {
+            System.out.println((i+1) + ". " + todoList.get(i));            
+        }
+    }
 }
